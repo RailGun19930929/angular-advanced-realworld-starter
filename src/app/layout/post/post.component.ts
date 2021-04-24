3b43151d7c298e97827111c9713e5ac44dfd1eef
JyunCun <JyunCun@smart-ecare.com>
2021-04-24 10:07:59 +0800

    任務03 ：在程式中取得路由參數

--- src/app/layout/post/post.component.ts
+++ src/app/layout/post/post.component.ts
@@ -1,4 +1,8 @@
+import { PostService } from './../../post.service';
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { SingleArticle } from 'src/app/interfaces/single-article';
+import { Article } from 'src/app/interfaces/article';
 
 @Component({
   selector: 'app-post',
@@ -7,9 +11,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PostComponent implements OnInit {
 
-  constructor() { }
+  article: Article;
+
+  constructor(
+    private route: ActivatedRoute,
+    private postService: PostService,
+  ) { }
 
   ngOnInit(): void {
+    this.route.paramMap
+      .subscribe((params) => {
+        const id = params.get('id');
+        console.log(`id = ${id}`);
+
+        this.postService.getArticle(id)
+          .subscribe((res: SingleArticle) => {
+            console.log(res.article);
+            this.article = res.article;
+          });
+      });
   }
 
 }
--- src/app/layout/posts/posts.component.ts
+++ src/app/layout/posts/posts.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Article } from 'src/app/interfaces/article';
+import { Articles } from 'src/app/interfaces/articles';
+import { PostService } from 'src/app/post.service';
 
 @Component({
   selector: 'app-posts',
@@ -7,9 +10,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PostsComponent implements OnInit {
 
-  constructor() { }
+  articles: Article[];
+
+  constructor(
+    private postService: PostService,
+  ) { }
 
   ngOnInit(): void {
+    this.postService.getArticles()
+    .subscribe((res: Articles) => {
+      console.log(res.articles);
+      this.articles = res.articles;
+    });
   }
 
 }
