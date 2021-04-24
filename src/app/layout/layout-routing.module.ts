9d690c9591a1601b1051468164d255b361dd1cc4
JyunCun <JyunCun@smart-ecare.com>
2021-04-24 10:53:05 +0800

    讓空路由移至下方，避免先載入其他模組

--- src/app/layout/layout-routing.module.ts
+++ src/app/layout/layout-routing.module.ts
@@ -6,10 +6,10 @@ const routes: Routes = [
   {
     path: '', component: LayoutComponent,
     children: [
-      { path: '', pathMatch: 'full', redirectTo: 'posts' },
       { path: 'posts', loadChildren: () => import('./posts/posts.module').then(m => m.PostsModule) },
       { path: 'post/:id', loadChildren: () => import('./post/post.module').then(m => m.PostModule) },
       { path: 'create', loadChildren: () => import('./create/create.module').then(m => m.CreateModule) },
+      { path: '', pathMatch: 'full', redirectTo: 'posts' },
     ],
   },
 ];
